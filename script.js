681273a5583c6d4acf2e6f040b57f3555785c27d
Andrei Scafi <andreiscafi@hotmail.com>
2023-02-08 05:41:53 -0300

    Adding background stars with JavaScript - Adicionando estrelas de fundo com JavaScript.

--- script.js
+++ script.js
@@ -125,12 +125,13 @@ class Projectile {
 
 //Creating a Particles:
 class Particle {
-    constructor({ position, velocity, radius, color }) {
+    constructor({ position, velocity, radius, color, fades }) {
         this.position = position;
         this.velocity = velocity;
         this.radius = radius;
         this.color = color;
         this.opacity = 1;
+        this.fades = fades;
     }
 
     draw() {
@@ -150,7 +151,7 @@ class Particle {
         this.position.y += this.velocity.y;
 
         //Fade out the particle opacity:
-        this.opacity -= 0.01
+        if (this.fades) this.opacity -= 0.01
     }
 }
 
@@ -315,8 +316,25 @@ const keys = {
 let frames = 0;
 let randomInterval = Math.floor((Math.random() * 500) + 500);
 
+//Create background star with particles:
+for (let i = 0; i < 100; i++) {
+    particles.push(new Particle({
+        position: {
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height
+        },
+        velocity: {
+            x: 0,
+            y: 0.3
+        },
+        radius: Math.random() * 2,
+        color: 'white'
+    })
+    )
+}
+
 //Create Particles Function:
-function createParticles({ object, color }) {
+function createParticles({ object, color, fades }) {
     //Create Particles
     for (let i = 0; i < 15; i++) {
         particles.push(new Particle({
@@ -328,8 +346,9 @@ function createParticles({ object, color }) {
                 x: (Math.random() - 0.5) * 2,
                 y: (Math.random() - 0.5) * 2
             },
-            radius: Math.random() * 2,
-            color: color || '#BAA0DE'
+            radius: Math.random() * 3,
+            color: color || '#BAA0DE',
+            fades: true
         })
         )
     }
@@ -345,6 +364,12 @@ function animate() {
 
     //Particles
     particles.forEach((particle, i) => {
+        //Background particles:
+        if (particle.position.y - particle.radius >= canvas.height) {
+            particle.position.x = Math.random() * canvas.width
+            particle.position.y = -particle.radius
+        }
+        //Explosion particles:
         if (particle.opacity <= 0) {
             setTimeout(() => {
                 particles.splice(i, 1)
@@ -373,7 +398,8 @@ function animate() {
             console.log('you lose')
             createParticles({
                 object: player,
-                color: 'white'
+                color: 'white',
+                fades: true
             })
         }
     })
@@ -420,7 +446,8 @@ function animate() {
                         // remove invader and projectiles
                         if (invaderFound && projectileFound) {
                             createParticles({
-                                object: invader
+                                object: invader,
+                                fades: true
                             })
 
                             grid.invaders.splice(i, 1)
